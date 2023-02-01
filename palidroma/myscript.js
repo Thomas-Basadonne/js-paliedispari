c4f7a091032db48363c75cb79db4ade5668a1c69
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-01 10:48:34 +0100

    final commit

--- palidroma/myscript.js
+++ palidroma/myscript.js
@@ -1,7 +1,14 @@
 // Palidroma
 // Chiedere all'utente di inserire una parola
 let parola = prompt("inserisci una parola");
-// Creare una funzione per capire se la parola inserita è palindroma
+/**
+ *
+ * @param {string} word parametro che contiene la parola da verificare
+ * @param {string|false} ritorna vero se la parola è palindroma altrimenti falso
+ *
+ *
+ * Creare una funzione per capire se la parola inserita è palindroma
+ */
 function controlloPalindroma(str) {
   let j = str.length - 1;
   for (let i = 0; i < j / 2; i++) {
@@ -19,9 +26,9 @@ function controlloPalindroma(str) {
 //function that print output if string is palindrome
 function parolaPalindroma(str) {
   // variable vera se la stringa è palindroma
-  let ans = controlloPalindroma(str);
+  let result = controlloPalindroma(str);
   //true or not
-  if (ans == true) {
+  if (result == true) {
     console.log("è palindroma!");
   } else {
     console.log("non è palindroma!");
@@ -32,3 +39,32 @@ function parolaPalindroma(str) {
 // parolaPalindroma(test);
 
 parolaPalindroma(parola);
+
+/***************************************************
+ *
+ *              correzione
+ *
+ **************************************************/
+const userWord = prompt("inserisci parola");
+
+if (isPalindrome(userWord)) {
+  console.log("è palindroma");
+} else {
+  console.log("non è palindroma");
+}
+
+function isPalindrome(word) {
+  let reverseWord = "";
+  for (let i = 0; i < word.length; i++) {
+    const currentLetter = word[i];
+    console.log(currentLetter);
+
+    reverseWord = currentLetter + reverseWord;
+    console.log(currentLetter);
+  }
+  console.log(reverseWord);
+
+  if (word == reverseWord) {
+    return "true";
+  }
+}
--- pari-dispari/myscript.js
+++ pari-dispari/myscript.js
@@ -1,6 +1,97 @@
 // Pari e Dispari
-// L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// L'utente sceglie pari o dispari
+
+var pariDispari = document.getElementById("evenOdd");
+var selectedIndex = pariDispari.selectedIndex;
+var valoreSelezionato = pariDispari.options[selectedIndex];
+console.log(valoreSelezionato);
+
+// e inserisce un numero da 1 a 5.
+let numeroUtente = parseInt(prompt("scegli un numero da 1 a 5!"));
+console.log(numeroUtente);
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
+function tiraDadi() {
+  let dado1 = numeroUtente;
+  let dado2 = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("dice").innerHTML = dado1 + " , " + dado2;
+  return dado1 + dado2;
+}
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+function risultato() {
+  let resto = tiraDadi() % 2;
+  if ((pariSelezionato == resto) == 0) {
+    document.getElementById("evenodd").innerHTML = "PARI";
+  } else {
+    document.getElementById("evenodd").innerHTML = "DISPARI";
+  }
+  return;
+}
+
+/************************************************************
+ *
+ *                      correzione
+ *
+ ************************************************************/
+
+// ON LOAD
+// utente sceglie pari o dispari
+const userChoice = prompt("scrivi 'pari' o 'dispari'");
+if (userChoice != "pari" && userChoice != "dispari") {
+  userChoice = "pari";
+  alert = "ho scelto pari per te!";
+}
+
+// l'utente sceglie un numero
+let userNumber = parseInt(prompt("scegli un numero da 1 a 5!"));
+
+// generiamo il numero random
+
+const pcNumber = generateRandomNumber(1, 5);
+
+// somma 2 numeri
+
+let gameTotal = userNumber + pcNumber;
+
+// stabiliamo somma pari o dispari
+let userWon = false;
+if (isEven(gameTotal)) {
+  if (userChoice == "pari") console.log("hai vinto!");
+  userWon = true;
+} else {
+  if (userChoice == "dispari") console.log("hai vinto!");
+  userWon = true;
+}
+
+if (userWon) {
+  console.log("hai vinto!");
+} else {
+  console.log("hai perso!");
+}
+
+// FUNCTIONS
+
+// Funzione che genera i numeri
+/** @param {int} min valore minimo generato */
+/** @param {int} max valore massimo generato */
+/** @return {int} numero randomico generato tra min e max */
+//
+
+function generateRandomNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
+
+// numero pari o dispari
+/** @param {int} num valore da verificare  */
+/** @param {boolean} pari o dispari */
+//
+
+function isEven(num) {
+  if (num % 2 == 0) {
+    return true;
+  }
+  return false;
+}
